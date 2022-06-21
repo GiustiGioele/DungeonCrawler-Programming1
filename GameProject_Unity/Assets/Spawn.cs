877f3b08b415928cac7e9c8e07f20c074736fa05
Walter Panico <85064350+walter1245@users.noreply.github.com>
2022-06-21 19:50:09 +0200

    Add spawner enemy and fix bug enemyAI

--- GameProject_Unity/Assets/Spawn.cs
+++ GameProject_Unity/Assets/Spawn.cs
@@ -1,18 +1,45 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+     public int lenght = 0;
+    [SerializeField] GameObject spawnPoints;
+    [SerializeField] GameObject enemy;
+    [SerializeField] float timer = 0;
+    [SerializeField] float spawnDelay = 1f;
+    public void SpawnEnemy()
     {
-        
-    }
+        int check = 0;
+        Debug.Log(check);
+        int randInt = RandomLenght();
 
-    // Update is called once per frame
-    void Update()
+        if (check < randInt)
+        {
+            timer += Time.deltaTime;
+            if (timer > spawnDelay)
+            {
+
+
+
+
+                GameObject.Instantiate(enemy, spawnPoints.transform.position, Quaternion.identity);
+                timer = 0;
+                check++;
+            }
+        }
+    }
+    public int RandomLenght()
     {
-        
+         var randInt = Random.Range(0, lenght);
+       return randInt;
+
+    }
+    private void Update()
+    {
+        SpawnEnemy();
     }
 }
+
--- GameProject_Unity/Assets/_game/Scripts/teleport.cs
+++ GameProject_Unity/Assets/_game/Scripts/teleport.cs
@@ -7,7 +7,21 @@ public class Teleport : MonoBehaviour
 {
     [SerializeField] GameObject target;
     [SerializeField] NavMeshAgent agent;
-
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Finish");
+    }
+    private void Update()
+    {
+        FollowEnemy();
+    }
+    void FollowEnemy()
+    {
+        if (target != null)
+        {
+            agent.destination = target.transform.position;
+        }
+    }
 
 }
 
